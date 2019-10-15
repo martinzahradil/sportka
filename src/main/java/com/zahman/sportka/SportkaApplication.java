1a1ce7beb153415de4cf67f92c11bf3940fe9a2c
Martin Zahradil <martin.zahradil@isonsoft.cz>
2019-10-15 23:00:40 +0200

    Initial commit

---
+++ src/main/java/com/zahman/sportka/SportkaApplication.java
@@ -0,0 +1,13 @@
+package com.zahman.sportka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportkaApplication.class, args);
+	}
+
+}
